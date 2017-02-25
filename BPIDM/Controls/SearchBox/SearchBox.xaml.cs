606cf796f7ec3af35263d0f19154890574ab21df
Kristopher Ruzic <krruzic@gmail.com>
2017-02-24 22:05:50 -0800

    Adds menu filtering via search bar

---
+++ BPIDM/Controls/SearchBox/SearchBox.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+
+namespace BPIDM.Controls
+{
+    /// <summary>
+    /// Interaction logic for SearchBox.xaml
+    /// </summary>
+    public partial class SearchBox : UserControl, INotifyPropertyChanged
+    {
+        private static DependencyProperty SearchTextProperty =
+                DependencyProperty.Register("SearchText", typeof(string), typeof(SearchBox),
+                new FrameworkPropertyMetadata(string.Empty, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault |
+                        FrameworkPropertyMetadataOptions.Inherits));
+
+        public string SearchText
+        {
+            get { return (string)GetValue(SearchTextProperty); }
+            set { SetValue(SearchTextProperty, value); }
+        }
+
+        public SearchBox()
+        {
+            InitializeComponent(); 
+            //DataContext = this;
+        }
+
+        private void OnPropertyChanged(string p)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(p));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void SearchBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            var t = (TextBox)sender;
+            t.SelectAll();
+        }
+
+        private void SearchTextBox_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            var t = (TextBox)sender;
+            t.SelectAll();
+        }
+
+        private void Search_Click(object sender, RoutedEventArgs e)
+        {
+            OnSearchEvent();
+        }
+
+        public static readonly RoutedEvent SearchEvent = EventManager.RegisterRoutedEvent(
+                "Search", // Event name
+                RoutingStrategy.Bubble, // Bubble means the event will bubble up through the tree
+                typeof(RoutedEventHandler), // The event type
+                typeof(SearchBox)
+            ); // Belongs to ChildControlBase
+
+        // Allows add and remove of event handlers to handle the custom event
+        public event RoutedEventHandler Search
+        {
+            add { AddHandler(SearchEvent, value); }
+            remove { RemoveHandler(SearchEvent, value); }
+        }
+
+        private void SearchTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key.Equals(Key.Enter))
+            {
+                OnSearchEvent();
+            } 
+        }
+
+        private void OnSearchEvent()
+        {
+            SearchText = SearchTextBox.Text;
+            var newEventArgs = new RoutedEventArgs(SearchBox.SearchEvent);
+            RaiseEvent(newEventArgs);
+        }
+    }
+}
--- BPIDM/Views/MainMenu.xaml.cs
+++ BPIDM/Views/MainMenu.xaml.cs
@@ -2,33 +2,102 @@
 using System.IO;
 using System.Windows.Controls;
 using System.Collections.ObjectModel;
+using System.Collections.Generic;
 using System.Windows.Media;
+using System.Windows.Data;
+using System.Windows;
 
 using Newtonsoft.Json;
 
 using BPIDM.Controls;
 using BPIDM.Utils;
+using System.ComponentModel;
 
 namespace BPIDM.Views
 {
     /// <summary>
     /// Interaction logic for MainMenu.xaml
     /// </summary>
-    public partial class MainMenu : UserControl, ISwitchable
+    public partial class MainMenu : UserControl, ISwitchable, INotifyPropertyChanged
     {
+        private string filterText;
+        public CollectionViewSource menuCollection;
+        public ICollectionView SourceCollection;
+
         public ObservableCollection<BPMenuItem> MenuContent { get; private set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public MainMenu()
         {
             InitializeComponent();
             MenuContent = new ObservableCollection<BPMenuItem>();
             FillList();
+
+            menuCollection = new CollectionViewSource();
+            ICollectionView SourceCollection = CollectionViewSource.GetDefaultView(MenuList.ItemsSource);
+            menuCollection.Source = MenuContent;
             this.DataContext = this;
         }
 
-        public void FillList()
+
+        public string FilterText
+        {
+            get { return filterText; }
+            set
+            {
+                filterText = value;
+                CollectionViewSource.GetDefaultView(MenuList.ItemsSource).Refresh();
+                RaisePropertyChanged("FilterText");
+            }
+        }
+
+        private void menuCollection_Filter(object sender, FilterEventArgs e)
+        {
+            if (string.IsNullOrEmpty(FilterText))
+            {
+                e.Accepted = true;
+                return;
+            }
+            BPMenuItem item = e.Item as BPMenuItem;
+            if (item.DisplayedName.ToUpper().Contains(FilterText.ToUpper()))
+            {
+                e.Accepted = true;
+            }
+            else
+            {
+                e.Accepted = false;
+            }
+        }
+
+        private void SearchBox_Search(object sender, FilterEventArgs e)
+        {
+            if (string.IsNullOrEmpty(FilterText))
+            {
+                e.Accepted = true;
+                return;
+            }
+
+            BPMenuItem item = e.Item as BPMenuItem;
+            if (item.DisplayedName.ToUpper().Contains(FilterText.ToUpper()))
+            {
+                e.Accepted = true;
+            }
+            else
+            {
+                e.Accepted = false;
+            }
+        }
+
+        public void RaisePropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void FillList()
         {
             string json = LoadJson();
             dynamic jsonObj = JsonConvert.DeserializeObject<RootMenuObject>(json);
+            int i = 0;
             foreach (var cat in jsonObj.Menu)
             {
                 foreach (var item in cat.Content)
@@ -41,21 +110,23 @@ namespace BPIDM.Views
                         DisplayedCategory = cat.CategoryName,
                     };
                     MenuContent.Add(bpitem);
+                    i++;
                 }
+            }
+            Console.WriteLine(i);
+        }
+
+        private string LoadJson()
+        {
+            using (StreamReader r = new StreamReader(@"Data/menu.json"))
+            {
+                return r.ReadToEnd();
             }
         }
 
         public void UtilizeState(object state)
         {
             throw new NotImplementedException();
-        }
-
-        public string LoadJson()
-        {
-            using (StreamReader r = new StreamReader(@"Data/menu.json"))
-            {
-               return r.ReadToEnd();
-            }
         }
     }
 }
