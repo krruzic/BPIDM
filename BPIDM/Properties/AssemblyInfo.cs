6703b2fde26a6cdf5f94a999b174b593740123aa
Kristopher Ruzic <krruzic@gmail.com>
2017-02-26 23:26:16 -0800

    Add a nice icon to the app
    also added a package so all the DLLs are packed into one EXE

--- BPIDM/Properties/AssemblyInfo.cs
+++ BPIDM/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BPIDM")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("An Interactive Menu for a popular Canadian Restaurant ;)")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BPIDM")]
